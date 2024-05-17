46befff28041c6cdad52f418cbf1b849f77970de
Gabriel <Gabrielpedrofc@gmail.com>
2024-05-16 22:35:51 -0300

    altera estrutura da tabela - refatorar front

--- src/db/SQL/posts.sql
+++ src/db/SQL/posts.sql
@@ -1,8 +1,23 @@
-CREATE TABLE tb_posts (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+-- Table: public.tb_posts
+
+-- DROP TABLE IF EXISTS public.tb_posts;
+
+CREATE TABLE IF NOT EXISTS public.tb_posts
+(
+    id integer NOT NULL DEFAULT nextval('tb_posts_id_seq'::regclass),
+    title character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    content character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    user_id integer NOT NULL,
+    main_content text COLLATE pg_catalog."default",
+    CONSTRAINT tb_posts_pkey PRIMARY KEY (id),
+    CONSTRAINT tb_posts_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.tb_posts
+    OWNER to postgres;
