7c5059d6851bcc82afb2fde5601ed229e4560d45
Gabriel <Gabrielpedrofc@gmail.com>
2024-05-26 11:10:36 -0300

    crud completo de produtos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,19 @@
 const pool = require('../db/database');
 
+const criarProduto = async (req, res) => {
+  try {
+    const { titulo, descricao , especificacoes_tecnicas, preco} = req.body;
+    const userId = req.usuario.id;
+    const newProduct = await pool.query(
+      'INSERT INTO tb_produtos (titulo, descricao, especificacoes_tecnicas, preco, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [titulo, descricao, especificacoes_tecnicas, preco, userId]
+    );
+    res.json(newProduct.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
 
 const obterProduto = async (req, res) => {
   try {
@@ -11,7 +25,54 @@ const obterProduto = async (req, res) => {
   }
 };
 
+const atualizarProduto = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { titulo, descricao, especificacoes_tecnicas, preco } = req.body;
+    const userId = req.usuario.id;
+
+    // Verificar se o produto pertence ao usuário autenticado
+    const produto = await pool.query('SELECT * FROM tb_produtos WHERE id = $1 AND user_id = $2', [id, userId]);
+    if (produto.rows.length === 0) {
+      return res.status(403).json('Ação proibida');
+    }
+
+    const updatedProduct = await pool.query(
+      'UPDATE tb_produtos SET titulo = $1, descricao = $2, especificacoes_tecnicas = $3, preco = $4 WHERE id = $5 RETURNING *',
+      [titulo, descricao, especificacoes_tecnicas, preco, id]
+    );
+    res.json(updatedProduct.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+
+const deletarProduto = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.usuario.id;
+
+    // Verificar se o produto pertence ao usuário autenticado
+    const produto = await pool.query('SELECT * FROM tb_produtos WHERE id = $1 AND user_id = $2', [id, userId]);
+    if (produto.rows.length === 0) {
+      return res.status(403).json('Ação proibida');
+    }
+
+    await pool.query('DELETE FROM tb_produtos WHERE id = $1', [id]);
+    res.json('Produto deletado com sucesso');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+
 
 module.exports = {
   obterProduto,
+  criarProduto,
+  atualizarProduto,
+  deletarProduto,
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { verificarToken } = require('../middlewares/authMiddleware');
-const { obterProduto } = require('../controllers/productController');
+const { obterProduto, criarProduto, atualizarProduto, deletarProduto } = require('../controllers/productController');
 
-router.get('/products', obterProduto);
+router.get('/products', verificarToken, obterProduto);
+router.post('/products', verificarToken, criarProduto);
+router.put('/products/:id', verificarToken, atualizarProduto);
+router.delete('/products/:id', verificarToken, deletarProduto);
 
 module.exports = router;
