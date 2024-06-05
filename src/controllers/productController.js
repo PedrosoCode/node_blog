4cf04e018aea3c3c20d0cfa87d5e2219886dbc2f
Gabriel <Gabrielpedrofc@gmail.com>
2024-06-05 20:51:04 -0300

    filtrar produtos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -26,13 +26,31 @@ const criarProduto = async (req, res) => {
 
 const obterProduto = async (req, res) => {
   try {
-    const allProducts = await pool.query('SELECT * FROM tb_produtos');
-    res.json(allProducts.rows);
+      const { title, minPrice, maxPrice } = req.query;
+      let query = 'SELECT * FROM tb_produtos WHERE 1=1';
+      let queryParams = [];
+
+      if (title) {
+          query += ' AND titulo ILIKE $' + (queryParams.length + 1);
+          queryParams.push(`%${title}%`);
+      }
+      if (minPrice) {
+          query += ' AND preco >= $' + (queryParams.length + 1);
+          queryParams.push(minPrice);
+      }
+      if (maxPrice) {
+          query += ' AND preco <= $' + (queryParams.length + 1);
+          queryParams.push(maxPrice);
+      }
+
+      const allProducts = await pool.query(query, queryParams);
+      res.json(allProducts.rows);
   } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
+      console.error(err.message);
+      res.status(500).send('Server Error');
   }
 };
+
 
 const obterProdutoPorId = async (req, res) => {
   try {
